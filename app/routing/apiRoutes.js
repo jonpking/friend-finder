f4f7806adff4527ca0131c1136e90c010787bfe6
Enigma <jonpking@gmail.com>
2019-08-14 20:29:39 -0700

    updated code to properly add survey info to friendArray

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,14 @@
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+const friendData = require("../data/friends");
 
 module.exports = (app) => {
     app.get("/api/friends", function (req, res) {
-        res.json(friends);
+        res.json(friendData);
     });
 
     app.post("/api/friends", function (req, res) {
-
+        let friend = req.body;
+        friendData.push(req.body);
+        //data handling logic
+        res.json(true);
     });
 }
