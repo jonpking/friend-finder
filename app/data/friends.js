d5061fa2e14c689990392096f0667b19e7fe01c8
Enigma <jonpking@gmail.com>
2019-08-17 22:33:53 -0700

    added test data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,84 @@
 const friendsArray = [
-
+    {
+        name: "test1name",
+        photo: "test1image",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            5,
+            3,
+            3,
+            1
+        ]
+    },
+    {
+        name: "test2name",
+        photo: "test2image",
+        scores: [
+            2,
+            2,
+            4,
+            5,
+            5,
+            2,
+            1,
+            2,
+            4,
+            5
+        ]
+    },
+    {
+        name: "test3name",
+        photo: "test3image",
+        scores: [
+            1,
+            1,
+            1,
+            2,
+            3,
+            4,
+            4,
+            5,
+            2,
+            2
+        ]
+    },
+    {
+        name: "test4name",
+        photo: "test4image",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            4,
+            5,
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        name: "test5name",
+        photo: "test5image",
+        scores: [
+            2,
+            2,
+            3,
+            4,
+            2,
+            4,
+            5,
+            2,
+            3,
+            2
+        ]
+    },
 ];
 
 module.exports = friendsArray;
